a304e04e186d8cb7455d0674fb703a1288dc16ed
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2023-05-09 11:21:28 -0400

    Create test_prepostestimationhelpers.R
    script to test prepostestimation functions

---
+++ inst/test_prepostestimationhelpers.R
@@ -0,0 +1,36 @@
+
+data("eusilcA_pop")
+data("eusilcA_smp")
+#### set of variables used in model estimation
+variables <- c("gender", "eqsize", "cash", "self_empl",
+              "unempl_ben", "age_ben", "surv_ben",
+              "sick_ben", "dis_ben", "rent", "fam_allow",
+              "house_allow", "cap_inv", "tax_adj")
+
+### estimate a unit model
+emdi_model <- emdiplus::ebp(fixed = as.formula(paste("eqIncome ~ ", paste(variables,
+                                                                         collapse= "+"))),
+                           pop_data = eusilcA_pop2,
+                           pop_domains = "district",
+                           smp_data = eusilcA_smp2,
+                           smp_domains = "district",
+                           na.rm = TRUE,
+                           weights = "weight",
+                           pop_weights = "popweights",
+                           MSE = TRUE,
+                           threshold = 11000,
+                           B = 2,
+                           L = 2)
+
+### model estimation
+ebp_reportdescriptives(ebp_object = emdi_model,
+                      smp_weights = "weight",
+                      pop_weights = "popweights",
+                      repvar = "state",
+                      welfare = "eqIncome",
+                      smp_data = eusilcA_smp2,
+                      pop_data = eusilcA_pop2,
+                      threshold = 11000,
+                      pop_domains = "district",
+                      smp_domains = "district",
+                      designvar = "district")
